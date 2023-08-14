366cef0f73daf8d7c3ca9ded9c76f5164cc16768
ahmetulvi <47754871+ahmetulvi@users.noreply.github.com>
2023-08-15 00:32:08 +0300

    Add files via upload

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,50 @@
-public class SortingBook implements Comparable<Book>{
+public class Book implements Comparable<Book>{
+    private String name;
+    private int pageNumber;
+    private String author;
+    private  String publishDate;
+
+    public Book(String name, int pageNumber, String author, String publishDate) {
+        this.name = name;
+        this.pageNumber = pageNumber;
+        this.author = author;
+        this.publishDate = publishDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return getName().compareTo(o.getName());
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
-public class Main {
+import java.util.TreeSet;
+
+
+public class  Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TreeSet<Book> books=new TreeSet<>();
+        Book b1=new Book("1984",450,"George Orwell","1949");
+        Book b2=new Book("Simyacı",350,"Paulo Coelho","1988");
+        Book b3=new Book("Zengin Baba Yoksul Baba",250,"Robert T. Kiyosaki","1997");
+        Book b4=new Book("Hayvan Çiftliği",320,"George Orwell","1945");
+        Book b5=new Book("Kürk Mantolu Madonna",430,"Sabahattin Ali","1943");
+
+        books.add(b1);
+        books.add(b2);
+        books.add(b3);
+        books.add(b4);
+        books.add(b5);
+
+
+        for (Book bookName:books){
+            System.out.println("Book Name: "+bookName.getName()+
+                    "\nNumber of Page: "+bookName.getPageNumber()+
+                    "\nAuthor: "+bookName.getAuthor()+
+                    "\nPublish Date: "+bookName.getPublishDate());
+            System.out.println("*********************************************");
+        }
+        System.out.println(" ");
+        TreeSet<Book> books1=new TreeSet<>(new OrderPageNumberComparator());
+        books.add(b1);
+        books.add(b2);
+        books.add(b3);
+        books.add(b4);
+        books.add(b5);
+
+        System.out.println("Order by Page Number");
+        for (Book bookPage:books1){
+            System.out.println("Book Name: "+bookPage.getName()+
+                    "\nNumber of Page: "+bookPage.getPageNumber()+
+                    "\nAuthor: "+bookPage.getAuthor()+
+                    "\nPublish Date: "+bookPage.getPublishDate());
+            System.out.println("*********************************************");
+        }
+
     }
 }
